b4ea1ed210c097386340fe4aa6eee409383bb984
endritber <endritpb@gmail.com>
2023-05-11 20:13:50 -0400

    added hr and lr patches __getitems__

--- data.py
+++ data.py
@@ -1,10 +1,12 @@
-#!/usr/bin/python3
+#!/usr/local/bin/python3
 import os
 
 import numpy as np
 import matplotlib.pyplot as plt
 import cv2
+import patchify
 
+import torch
 from torch.utils.data import Dataset
 from torchvision.transforms import functional as F
 
@@ -14,42 +16,31 @@ def show_resolution_image(image):
   plt.imshow(image)
   plt.show()
   
-def get_image_file_names(path):
-  files = [os.path.join(path, image_name) for image_name in os.listdir(path)]
-  return files
 
-
+def get_patches(path):
+  lr_patches = [patch for patch in os.listdir(os.path.join(path, 'lr_patches'))]
+  hr_patches = [patch for patch in os.listdir(os.path.join(path, 'hr_patches'))]
+  return lr_patches, hr_patches
 
 class SuperResolutionDataset(Dataset):
-  def __init__(self, path='datasets/T91') -> None:
+  def __init__(self, path='datasets/T91/sub', upscale_factor=3) -> None:
     super().__init__()
-    self.images = get_image_file_names(path)
+    self.lr_patches, self.hr_patches = get_patches(path)
+    self.upscale_factor = upscale_factor
+    self.path = path
     
   def __len__(self) -> int:
-    return len(self.images)
+    return len(self.lr_patches)
   
   def __getitem__(self, index):
-    image = cv2.imread(self.images[index], cv2.IMREAD_UNCHANGED).astype(np.float32) / 255.
-    interpolated_image = cv2.resize(image, (240, 240), interpolation=cv2.INTER_CUBIC)
-    # interpolated_image = bicubic(image, self.upscale_factor, 1)  
-    print(interpolated_image.shape)
-    tensor = F.to_tensor(interpolated_image)
-    return tensor
-  
+    lr_patch, hr_patch = cv2.imread(os.path.join(self.path, 'lr_patches', self.lr_patches[index])), cv2.imread(os.path.join(self.path, 'hr_patches', self.hr_patches[index])) 
+    return lr_patch.astype(np.float32)/255., hr_patch.astype(np.float32)/255.
     
 if __name__ == '__main__':
-  # show_resolution_image(cv2.imread(os.path.join('datasets/T91', 't1.png')))
-  
   dataset = SuperResolutionDataset()
   mean_x = []
   mean_y = []
   for x in dataset:
-    # print(x)
-    mean_x.append(x.shape[0])
-    mean_y.append(x.shape[1])
-  
-  print('x', sum(mean_x)/len(mean_x), 'y', sum(mean_y)/len(mean_y))
-    
-  
+    pass
   
   
